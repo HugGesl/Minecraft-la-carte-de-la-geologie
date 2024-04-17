f981f45dfd64093df41ab39e3807fc9d534af5da
HugGesl <hugo.geslin@laposte.net>
2024-04-17 17:10:51 +0200

    ajout 16 type semantic

--- minalac-generator-geol/src/main/java/com/ignfab/minalac/generator/minetest/MTVoxelTypeFactory.java
+++ minalac-generator-geol/src/main/java/com/ignfab/minalac/generator/minetest/MTVoxelTypeFactory.java
@@ -24,6 +24,36 @@ public class MTVoxelTypeFactory implements VoxelTypeFactory {
                 return new MTSimpleVoxelType(this.world, "default:dirt");
             case Water:
                 return new MTSimpleVoxelType(this.world, "default:water_source");
+            case White:
+                return new MTSimpleVoxelType(this.world, "wool:white");
+            case Grey:
+                return new MTSimpleVoxelType(this.world, "wool:grey");
+            case Dark_Grey:
+                return new MTSimpleVoxelType(this.world, "wool:dark_grey");
+            case Black:
+                return new MTSimpleVoxelType(this.world, "wool:black");
+            case Blue:
+                return new MTSimpleVoxelType(this.world, "wool:blue");
+            case Cyan:
+                return new MTSimpleVoxelType(this.world, "wool:cyan");
+            case Green:
+                return new MTSimpleVoxelType(this.world, "wool:green");
+            case Dark_Green:
+                return new MTSimpleVoxelType(this.world, "wool:dark_green");
+            case Yellow:
+                return new MTSimpleVoxelType(this.world, "wool:yellow");
+            case Orange:
+                return new MTSimpleVoxelType(this.world, "wool:orange");
+            case Brown:
+                return new MTSimpleVoxelType(this.world, "wool:brown");
+            case Red:
+                return new MTSimpleVoxelType(this.world, "wool:red");
+            case Pink:
+                return new MTSimpleVoxelType(this.world, "wool:pink");
+            case Magenta:
+                return new MTSimpleVoxelType(this.world, "wool:magenta");
+            case Violet:
+                return new MTSimpleVoxelType(this.world, "wool:violet");
             default:
                 return new MTSimpleVoxelType(this.world, "air");
         }
