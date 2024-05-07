aec7313c74ae4d55ceb5d004c93b48ea9ee928ef
HugGesl <hugo.geslin@laposte.net>
2024-05-07 14:10:08 +0200

    ajout générateur image minimap

--- minalac-generator-geol/src/main/java/com/ignfab/minalac/generator/SampleImplementation.java
+++ minalac-generator-geol/src/main/java/com/ignfab/minalac/generator/SampleImplementation.java
@@ -8,6 +8,7 @@ import it.geosolutions.jaiext.iterators.RandomIterFactory;
 
 import java.awt.Dimension;
 import java.awt.Rectangle;
+import java.awt.image.RenderedImage;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -18,15 +19,19 @@ import java.util.Map;
 import java.util.Set;
 import java.util.HashMap;
 
+import javax.imageio.ImageIO;
 import javax.media.jai.iterator.RandomIter;
 
 import org.geotools.api.data.FileDataStore;
 import org.geotools.api.data.FileDataStoreFinder;
 import org.geotools.api.data.SimpleFeatureSource;
+import org.geotools.api.geometry.Position;
 import org.geotools.api.referencing.crs.CoordinateReferenceSystem;
 import org.geotools.coverage.grid.GridCoverage2D;
+import org.geotools.coverage.grid.GridEnvelope2D;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.util.NullProgressListener;
+import org.geotools.geometry.Position2D;
 import org.geotools.geometry.jts.ReferencedEnvelope;
 import org.geotools.process.ProcessException;
 import org.geotools.process.vector.VectorToRasterProcess;
@@ -43,7 +48,7 @@ public class SampleImplementation {
          long startTime = System.currentTimeMillis();
          //Parent directory must exist
          //Example : "/home/john/.minetest/worlds/map/"
-         String directoryFullPath = "C:\\Users\\vmn\\Documents\\ENSG\\2023-2024\\PDI\\minetest-5.8.0-win64\\minetest-5.8.0-win64\\worlds\\minecraft";
+         String directoryFullPath = "C:/Users/Moi/Documents/PDI/minetest-5.8.0-win64/worlds/toto";
 
 
     	// The function createWorldFromUrl doesn't, for now, handle the parameters.
@@ -51,7 +56,7 @@ public class SampleImplementation {
     	// BBOX has to be a square.
     	// Width and height have to be one-tenth of the side of the side length of the BBOX's square
     	// Example "https://data.geopf.fr/wms-r/wms?LAYERS=RGEALTI-MNT_PYR-ZIP_FXX_LAMB93_WMS&FORMAT=image/x-bil;bits=32&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&STYLES=&CRS=EPSG:2154&BBOX=595000,6335000,605000,6345000&WIDTH=1000&HEIGHT=1000"
-    	String dataUrl = "https://data.geopf.fr/wms-r/wms?LAYERS=RGEALTI-MNT_PYR-ZIP_FXX_LAMB93_WMS&FORMAT=image/x-bil;bits=32&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&STYLES=&CRS=EPSG:2154&BBOX=704662,6584242,714662,6594242&WIDTH=1000&HEIGHT=1000";
+    	String dataUrl = "https://data.geopf.fr/wms-r/wms?LAYERS=RGEALTI-MNT_PYR-ZIP_FXX_LAMB93_WMS&FORMAT=image/x-bil;bits=32&SERVICE=WMS&VERSION=1.3.0&REQUEST=GetMap&STYLES=&CRS=EPSG:2154&BBOX=886723,6543233,887723,6544233&WIDTH=1000&HEIGHT=1000";
     	
     	
     	float[] mntArray = getHeightMap(dataUrl);
@@ -72,19 +77,18 @@ public class SampleImplementation {
         // Get bbox values
         String bboxStr = params.get("BBOX");
         String[] bboxValues = bboxStr.split(",");
-        double xmin = Double.parseDouble(bboxValues[0]);
-        double ymin = Double.parseDouble(bboxValues[1]);
-        double xmax = Double.parseDouble(bboxValues[2]);
-        double ymax = Double.parseDouble(bboxValues[3]);
-        System.out.println(xmax);
+        int xmin = Integer.parseInt(bboxValues[0]);
+        int ymin = Integer.parseInt(bboxValues[1]);
+        int xmax = Integer.parseInt(bboxValues[2]);
+        int ymax = Integer.parseInt(bboxValues[3]);
 
 
         // Get width and height
         int width = Integer.parseInt(params.get("WIDTH"));
         int height = Integer.parseInt(params.get("HEIGHT"));
         
-        System.out.println(height);
-        System.out.println("Done");
+    	System.out.println(xmin +" "+ xmax +" "+ height);
+
 
 
     	// Load shapefile
@@ -92,6 +96,7 @@ public class SampleImplementation {
     	SimpleFeatureSource featureSource = store.getFeatureSource();
     	SimpleFeatureCollection collection = featureSource.getFeatures();
     	CoordinateReferenceSystem CRS = featureSource.getSchema().getCoordinateReferenceSystem();
+    	System.out.println("Shapefile load");
 
     	// Get unique elements
     	AttributionType attributionType = new AttributionType();
@@ -107,12 +112,16 @@ public class SampleImplementation {
     	Dimension gridDim = new Dimension(width, height);
     	// Output name for raster data
     	String output = "MapGeol";
+    
     	// Progress monitor
     	NullProgressListener monitor = new NullProgressListener();
 
     	// Convert vector data to raster
     	GridCoverage2D sorted = VectorToRasterProcess.process(collection, attributeName, gridDim, bounds, output, monitor);
-
+    	RenderedImage grid = sorted.getRenderedImage();
+    	
+    	 System.out.println("Rasterisation complete");
+    	 
     	// Create voxel world
     	VoxelWorld worldMnt = new MTVoxelWorld();
     	createWorldFromMnt(mntArray, worldMnt, sorted, codeLegToSemanticType);
@@ -121,6 +130,12 @@ public class SampleImplementation {
     	// Set spawn point
         int midPoint = (int) (mntArray.length + Math.sqrt(mntArray.length)) / 2;
         setStaticSpawnPoint(directoryFullPath, 0, (int) mntArray[midPoint] / 10 + 1, 0);
+        
+        //Create minimap
+        MinimapPicture mp = new MinimapPicture(xmin, ymin, xmax, ymax, width, height, "2154" , "png");
+        mp.saveImage("../output.png");
+        
+        
         long endTime = System.currentTimeMillis();
      // Calculer la durée écoulée en millisecondes
         long elapsedTime = endTime - startTime;
@@ -140,15 +155,14 @@ public class SampleImplementation {
 
         int worldLength = (int) Math.sqrt(mntArray.length);
         
-        System.out.println(worldLength);
-        
+
         int x, y, z;
         VoxelType stoneVT = world.getFactory().createVoxelType(SemanticType.Stone);
 
         Rectangle bounds = new Rectangle(0,0,1000,1000);
         RandomIter iterator = RandomIterFactory.create(codeLegGrid.getRenderedImage(), bounds, true, true);
 
-      
+        System.out.println("Map en cours de creation");
         for (int i = 0; i < mntArray.length; i++) {
             //Temporary translation so the player spawn at the center of the generated map (player info isn't generated yet on this version)
             x = i % worldLength - worldLength / 2;
@@ -157,10 +171,6 @@ public class SampleImplementation {
             y = (int) mntArray[i];
             z = i / worldLength - worldLength / 2;
             
-            if (i%123456 == 0) {
-            	//afin de connaître l'altitude où se placer quand on arrive sur minetest.
-            	System.out.println(x+" "+y+" "+z);
-            }
             
             int xgrid = i % worldLength;
             int ygrid = i / worldLength;
@@ -173,16 +183,11 @@ public class SampleImplementation {
             else {BlockX = world.getFactory().createVoxelType(BlockType);
             }
             
-            
+           
             BlockX.place(x, y, z);
             BlockX.place(x, (y - 1), z);
             BlockX.place(x, (y - 2), z);
 
-
-            for (int y_bis = y - 3; y_bis > y - (3 + 10); y_bis--) {
-            	BlockX.place(x, y_bis, z);
-
-            }
         }
     }
     
